8c88a8f6c4369ee5d66d99701b8d0eec436d2049
sean <sean@seansullivan.com>
2010-03-08 07:22:28 +0000

    added fields and setters / getters

--- meetup-java-client/src/main/meetup/Rsvp.java
+++ meetup-java-client/src/main/meetup/Rsvp.java
@@ -1,8 +1,154 @@
 
 package meetup;
 
+import java.util.*;
+
 public class Rsvp
 {
+	private String name;
+	private String link;
+	private String comment;
+	private String latitude;
+	private String longitude;
+	private String city;
+	private String country;
+	private String state;
+	private String answers;
+	private Calendar created;
+	private Calendar updated;
+
+	
+	public String getName()
+	{
+		return name;
+	}
+
+
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+
+
+	public String getLink()
+	{
+		return link;
+	}
+
+
+	public void setLink(String link)
+	{
+		this.link = link;
+	}
+
+
+	public String getComment()
+	{
+		return comment;
+	}
+
+
+	public void setComment(String comment)
+	{
+		this.comment = comment;
+	}
+
+
+	public String getLatitude()
+	{
+		return latitude;
+	}
+
+
+	public void setLatitude(String latitude)
+	{
+		this.latitude = latitude;
+	}
+
+
+	public String getLongitude()
+	{
+		return longitude;
+	}
+
+
+	public void setLongitude(String longitude)
+	{
+		this.longitude = longitude;
+	}
+
+
+	public String getCity()
+	{
+		return city;
+	}
+
+
+	public void setCity(String city)
+	{
+		this.city = city;
+	}
+
+
+	public String getCountry()
+	{
+		return country;
+	}
+
+
+	public void setCountry(String country)
+	{
+		this.country = country;
+	}
+
+
+	public String getState()
+	{
+		return state;
+	}
+
+
+	public void setState(String state)
+	{
+		this.state = state;
+	}
+
+
+	public String getAnswers()
+	{
+		return answers;
+	}
+
+
+	public void setAnswers(String answers)
+	{
+		this.answers = answers;
+	}
+
+
+	public Calendar getCreated()
+	{
+		return created;
+	}
+
+
+	public void setCreated(Calendar created)
+	{
+		this.created = created;
+	}
+
+
+	public Calendar getUpdated()
+	{
+		return updated;
+	}
+
+
+	public void setUpdated(Calendar updated)
+	{
+		this.updated = updated;
+	}
+
 
 	public String toString()
 	{
