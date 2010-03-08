518c20cb302eef0ba0419b47d1faadef971ec075
sean <sean@seansullivan.com>
2010-03-08 07:25:16 +0000

    added fields and setters / getters

--- meetup-java-client/src/main/meetup/Photo.java
+++ meetup-java-client/src/main/meetup/Photo.java
@@ -1,8 +1,104 @@
 
 package meetup;
 
+import com.thoughtworks.xstream.annotations.XStreamAlias;
+import java.util.*;
+
 public class Photo
 {
+	private String albumTitle;
+	private String link;
+	private String albumId;
+	private String memberUrl;
+	
+	@XStreamAlias("desc")
+	private String description;
+	
+	private Calendar created;
+	
+	public String getAlbumTitle()
+	{
+		return albumTitle;
+	}
+
+
+
+	public void setAlbumTitle(String albumTitle)
+	{
+		this.albumTitle = albumTitle;
+	}
+
+
+
+	public String getLink()
+	{
+		return link;
+	}
+
+
+
+	public void setLink(String link)
+	{
+		this.link = link;
+	}
+
+
+
+	public String getAlbumId()
+	{
+		return albumId;
+	}
+
+
+
+	public void setAlbumId(String albumId)
+	{
+		this.albumId = albumId;
+	}
+
+
+
+	public String getMemberUrl()
+	{
+		return memberUrl;
+	}
+
+
+
+	public void setMemberUrl(String memberUrl)
+	{
+		this.memberUrl = memberUrl;
+	}
+
+
+
+	public String getDescription()
+	{
+		return description;
+	}
+
+
+
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+
+
+
+	public Calendar getCreated()
+	{
+		return created;
+	}
+
+
+
+	public void setCreated(Calendar created)
+	{
+		this.created = created;
+	}
+
+
 
 	public String toString()
 	{
