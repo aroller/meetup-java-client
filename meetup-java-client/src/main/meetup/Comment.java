aa8a6f98ea6541c6d67d3cf6c626fbd79f80ec43
sean <sean@seansullivan.com>
2010-03-10 07:29:44 +0000

    added setter/getter

--- meetup-java-client/src/main/meetup/Comment.java
+++ meetup-java-client/src/main/meetup/Comment.java
@@ -3,6 +3,29 @@ package meetup;
 
 public class Comment
 {
+	private String text;
+	
+	public Comment()
+	{
+		super();
+	}
+	
+	public Comment(String s)
+	{
+		this.setText(s);
+	}
+	
+	
+
+	public String getText()
+	{
+		return text;
+	}
+
+	public void setText(String s)
+	{
+		this.text = s;
+	}
 
 	public String toString()
 	{
